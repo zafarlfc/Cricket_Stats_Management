049c82ee06a7fee1f37a5aedb506fa01b15e8a01
Zafar Iqbal <zafarlfc>
2022-03-10 23:52:02 +0500

    JavaScript Reference types

--- index.js
+++ index.js
@@ -18,3 +18,38 @@ console.log(typeof number_);
 console.log(typeof is_eligible);
 console.log(typeof not_defined);
 console.log(typeof selection);
+
+
+// Reference Types
+
+// 1. Objects
+
+let company = {
+    name: "SMC",
+    id: 1234
+};
+console.log(company);
+
+company.name = "Private";
+console.log(company.name);
+
+company["name"] = "Public";
+console.log(company.name);
+
+let select = "name";
+company[select] = "SMC";
+console.log(company.name);
+
+// Arrays
+
+let cart = ["item1", "item2"];
+console.log(cart);
+console.log(cart[0]);
+
+cart[2] = "item3";
+console.log(cart);
+
+cart[3] = 4;
+console.log(cart);
+
+console.log(cart.length);
