e2ab61a8c7b41a410493acd6b09234b14c59f6ac
Zafar Iqbal <zafarlfc>
2022-03-12 14:39:34 +0500

    OOP in JavaScript

---
+++ OOP.js
@@ -0,0 +1,19 @@
+// Encapsulation      (Reduce complexity + increase reusability)
+
+let Person = {
+    name: "Someone",
+    age: 20,
+    get_age: function(){
+        return this.age;
+    }
+};
+
+Person.get_age();
+
+// Abstraction          (Reduce complexity + isolate impact of change)
+
+
+// Inheritance          (Eliminate redundant code)
+
+
+// Polymorphism         (Refactor ugly switch/case statements)
