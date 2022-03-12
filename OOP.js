cc6d0cd1b87944d6f20606dabbd86b34daa7c98f
Zafar Iqbal <zafarlfc>
2022-03-12 23:44:36 +0500

    Accessing key value of objects

--- OOP.js
+++ OOP.js
@@ -52,3 +52,29 @@ my_number1.square();
 my_number1.name
 my_number1.length
 my_number1.constructor
+
+
+// Primitive types are copied by their value
+// Objects are copied by thier reference
+
+const number2 = new GetSquare(2);
+
+for (let key in number2) {
+    console.log(key);
+}
+
+for (let key in number2) {
+    console.log(key, number2[key]);
+}
+
+for (let key in number2) {
+    if (typeof number2[key] !== "function")
+        console.log(key, number2[key]);
+}
+
+
+const keys = Object.keys(number2);
+console.log(keys);
+
+if ("number" in number2)
+    console.log("Key number exists!");
