f027d0f05ba0e5ddbff71d47220b8dbb0214403e
zafarlfc <zafarlfc@outlook.com>
2024-06-03 23:53:51 +0500

    django views implementation

--- my_django_app/my_app/views.py
+++ my_django_app/my_app/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello")
+
+def response(request):
+    path = request.path
+    return HttpResponse(path, content_type='text/html', charset='utf-8')
+
