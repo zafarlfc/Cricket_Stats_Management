e0616af2fb08365561a2ad8ec1e99725265842dd
zafarlfc <zafarlfc@outlook.com>
2024-06-15 15:40:23 +0500

    implemented simple django form

--- my_django_app/my_app/forms.py
+++ my_django_app/my_app/forms.py
@@ -1,12 +1,5 @@
-from django.db import models
 from django import forms
 
-# Create your models here.
-
-class Menu(models.Model):
-    name = models.CharField(max_length=100)
-    cuisine = models.CharField(max_length=100)
-    price = models.IntegerField()
 
 class DemoForm(forms.Form):
     name = forms.CharField(widget=forms.Textarea(attrs={"rows": 5}))
--- my_django_app/my_app/views.py
+++ my_django_app/my_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+
+from .models import DemoForm
 
 def home(request):
     return HttpResponse("Hello")
@@ -10,3 +11,7 @@ def response(request):
     path = request.path
     return HttpResponse(path, content_type='text/html', charset='utf-8')
 
+def form(request):
+    form = DemoForm()
+    context = {"form": form}
+    return render(request, template_name="form.html", context=context)
