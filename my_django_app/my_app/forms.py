d68c80d46a1dbf32dfa49c0ebf4e7024e422dd6f
Zafar <zafarlfc@outlook.cpm>
2026-04-07 01:11:32 +0500

    added project

--- my_django_app/my_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class DemoForm(forms.Form):
-    name = forms.CharField(widget=forms.Textarea(attrs={"rows": 5}))
-    email = forms.EmailField(label="Enter email address")
-    date = forms.DateField(widget=forms.widgets.NumberInput(attrs={"type": "date"}))
--- my_django_app/my_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home),
-    path('form/', views.form),
-    path('menu/', views.languages),
-]
