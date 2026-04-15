e43d10b9a861977655693edc5a373428941195d8
Zafar Iqbal <zafarlfc@outlook.com>
2026-04-16 00:58:47 +0500

    added players app

--- players/serializers.py
+++ players/serializers.py
@@ -3,7 +3,25 @@ from rest_framework import serializers
 from .models import *
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'text', 'created_at']
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Rating
+        fields = ['id', 'user', 'score']
+
+
 class PlayerSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    ratings = RatingSerializer(many=True, read_only=True)
     average_rating = serializers.SerializerMethodField()
 
     class Meta:
@@ -11,21 +29,7 @@ class PlayerSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_average_rating(self, obj):
-        ratings = Rating.objects.all()
+        ratings = obj.ratings.all()
         if ratings.exists():
             return round(sum(r.score for r in ratings) / ratings.count(), 2)
         return 0
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = '__all__'
-
-
-class RatingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rating
-        fields = '__all__'
